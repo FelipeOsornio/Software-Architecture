dd5baa9aa634e3be3cccfd4a5465157dc2308429
Felipe Osornio <felipe.osrn@gmail.com>
2020-04-27 23:16:14 -0500

    UML diagram upload

--- Arquitectura de software/dsl/src/jankenpon.rb
+++ Arquitectura de software/dsl/src/jankenpon.rb
@@ -18,14 +18,7 @@ class Sum
   # Method that call the evaluation method of the first object
   # and using the second one as parameter
   def evaluate
-    resultAB = @exp_a.evaluate(@exp_b)
-    resultBA = @exp_b.evaluate(@exp_a)
-
-    if resultAB
-      @exp_a
-    else
-      @exp_b
-    end
+    @exp_a.evaluate(@exp_b) ? @exp_a : @exp_b
   end
 end
 
@@ -42,14 +35,7 @@ class Subtraction
   # Method that call the evaluation method of the first object
   # and using the second one as parameter
   def evaluate
-    resultAB = @exp_a.evaluate(@exp_b)
-    resultBA = @exp_b.evaluate(@exp_a)
-
-    if resultAB
-      @exp_b
-    else
-      @exp_a
-    end
+    @exp_a.evaluate(@exp_b) ? @exp_b : @exp_a
   end
 end
 
@@ -67,9 +53,11 @@ class Scissors
   # interpreter operation.
   def self.+(object)
     if object == Spock
-      puts "Spock smashes Scissors (loser Scissors)\n"
+      puts "Spock smashes Scissors (winner Spock)\n"
     elsif object == Rock
-      puts "Rock crushes Scissors (loser Scissors)\n"
+      puts "Rock crushes Scissors (winner Rock)\n"
+    elsif object == Lizard
+      puts "Scissors decapitate Lizard (winner Scissors)\n"
     elsif object == Scissors
       puts "Scissors tie (winner Scissors)\n"
     end
@@ -81,11 +69,11 @@ class Scissors
   # interpreter operation.
   def self.-(object)
     if object == Paper
-      puts "Scissors cut Paper (winner Scissors)\n"
+      puts "Scissors cut Paper (loser Paper)\n"
     elsif object == Lizard
-      puts "Scissors decapitate Lizard (winner Scissors)\n"
+      puts "Scissors decapitate Lizard (loser Lizard)\n"
     elsif object == Scissors
-      puts "Scissors tie (winner Scissors)\n"
+      puts "Scissors tie (loser Scissors)\n"
     end
 
     Subtraction.new(self, object).evaluate
@@ -105,10 +93,14 @@ class Paper
   # on the object that receives as argument, and then executes the the sum
   # interpreter operation.
   def self.+(object)
-    if object == Scissors
-      puts "Scissors cut Paper (loser Paper)\n"
+    if object == Rock
+      puts "Paper covers Rock (winner Paper)\n"
+    elsif object == Scissors
+      puts "Scissors cut Paper (winner Scissors)\n"
     elsif object == Lizard
-      puts "Lizard eats Paper (loser Paper)\n"
+      puts "Lizard eats Paper (winner Lizard)\n"
+    elsif object == Spock
+      puts "Paper disproves Spock (winner Paper)\n"
     elsif object == Paper
       puts "Paper tie (winner Paper)\n"
     end
@@ -121,12 +113,17 @@ class Paper
   # interpreter operation.
   def self.-(object)
     if object == Rock
-      puts "Paper covers Rock (winner Paper)\n"
+      puts "Paper covers Rock (loser Rock)\n"
+    elsif object == Scissors
+      puts "Scissors cut Paper (loser Paper)\n"
     elsif object == Spock
-      puts "Paper disproves Spock (winner Paper)\n"
+      puts "Paper disproves Spock (loser Spock)\n"
+    elsif object == Lizard
+      puts "Lizard eats Paper (loser Paper)\n"
     elsif object == Paper
-      puts "Paper tie (winner Paper)\n"
+      puts "Paper tie (loser Paper)\n"
     end
+
     Subtraction.new(self, object).evaluate
   end
 end
@@ -138,7 +135,6 @@ class Rock
   # object passed as an argument
   def self.evaluate(object)
     object == Scissors || object == Lizard
-
   end
 
   # Method that defines the output string of the sum operation depending
@@ -146,9 +142,9 @@ class Rock
   # interpreter operation.
   def self.+(object)
     if object == Paper
-      puts "Paper covers Rock (loser Rock)\n"
+      puts "Paper covers Rock (winner Paper)\n"
     elsif object == Spock
-      puts "Spock vaporizes Rock (loser Rock)\n"
+      puts "Spock vaporizes Rock (winner Spock)\n"
     elsif object == Rock
       puts "Rock tie (winner Rock)\n"
     end
@@ -161,11 +157,13 @@ class Rock
   # interpreter operation.
   def self.-(object)
     if object == Lizard
-      puts "Rock crushes Lizard (winner Rock)\n"
+      puts "Rock crushes Lizard (loser Lizard)\n"
+    elsif object == Paper
+      puts "Paper covers Rock (loser Rock)\n"
     elsif object == Scissors
-      puts "Rock crushes Scissors (winner Rock)\n"
+      puts "Rock crushes Scissors (loser Scissors)\n"
     elsif object == Rock
-      puts "Rock tie (winner Rock)\n"
+      puts "Rock tie (loser Rock)\n"
     end
     Subtraction.new(self, object).evaluate
   end
@@ -185,11 +183,11 @@ class Lizard
   # interpreter operation.
   def self.+(object)
     if object == Scissors
-      puts "Scissors decapitate Lizard (loser Lizard)\n"
+      puts "Scissors decapitate Lizard (winner Scissors)\n"
     elsif object == Rock
-      puts "Rock crushes Lizard (loser Lizard)\n"
+      puts "Rock crushes Lizard (winner Rock)\n"
     elsif object == Lizard
-      puts "Lizard tie (loser Lizard)\n"
+      puts "Lizard tie (winner Lizard)\n"
     end
 
     Sum.new(self, object).evaluate
@@ -200,11 +198,11 @@ class Lizard
   # interpreter operation.
   def self.-(object)
     if object == Spock
-      puts "Lizard poisons Spock (winner Lizard)\n"
+      puts "Lizard poisons Spock (loser Spock)\n"
     elsif object == Paper
-      puts "Lizard eats Paper (winner Lizard)\n"
+      puts "Lizard eats Paper (loser Paper)\n"
     elsif object == Lizard
-      puts "Lizard tie (winner Lizard)\n"
+      puts "Lizard tie (loser Lizard)\n"
     end
 
     Subtraction.new(self, object).evaluate
@@ -224,10 +222,14 @@ class Spock
   # on the object that receives as argument, and then executes the the sum
   # interpreter operation.
   def self.+(object)
-    if object == Scissor
+    if object == Scissors
       puts "Spock smashes Scissors (winner Spock)\n"
+    elsif object == Lizard
+      puts "Lizard poisons Spock (winner Lizard)\n"
     elsif object == Rock
       puts "Spock vaporizes Rock (winner Spock)\n"
+    elsif object == Paper
+      puts "Paper disproves Spock (winner Paper)\n"
     elsif object == Spock
       puts "Spock tie (winner Spock)\n"
     end
@@ -255,6 +257,5 @@ def show(object)
   puts "Result = #{object}\n"
 end
 
-show Spock - Paper
 
 
